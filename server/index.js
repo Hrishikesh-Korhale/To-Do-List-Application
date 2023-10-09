7ddabd2f20cd7798ac6b36118294d7754c37af5a
Hrishi <hrishikorhale2002@gmail.com>
2023-10-09 15:08:29 +0530

    Setup Route for Todo

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import Connection from "./Database/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
+import Routes from "./Routes/route.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
 dotenv.config();
+
+app.use(cors());
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", Routes);
 
 const PORT = 8000;
 
